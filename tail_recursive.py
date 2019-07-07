a22bdeaca26d992d66f3401939251b437fac0e42
Judekeyser <justin.dekeyser@gmail.com>
2019-07-07 23:19:04 +0200

    Tail recursive module file

---
+++ tail_recursive.py
@@ -0,0 +1,32 @@
+# This is the module file for the implementation of tail recursion in Python
+
+class ContinueState:
+	pass
+
+
+class Proxy:
+	def __init__(self, wrapped):
+		self.first_time = True
+		self.wrapped = wrapped
+		self.CONTINUE = ContinueState()
+		self.stored_args = None
+		self.stored_kwargs = None
+	
+	def do(self, args, kwargs):
+		self.stored_args = args
+		self.stored_kwargs = kwargs
+		if self.first_time:
+			self.first_time = False
+			x = self.CONTINUE
+			while x == self.CONTINUE:
+				x = self.wrapped(*self.stored_args, **self.stored_kwargs)
+			return x
+		return self.CONTINUE
+
+
+def tail_recursive(f):
+	proxy = Proxy(f)
+	def g(*args, **kwargs):
+		return proxy.do(args, kwargs)
+	return g
+
